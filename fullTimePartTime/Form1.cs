6cb9002e52f71135dee534370f90a359efa56937
yu2519hn <1234568hH*>
2020-02-19 20:30:08 -0600

    comments entered

--- fullTimePartTime/Form1.cs
+++ fullTimePartTime/Form1.cs
@@ -19,27 +19,27 @@ namespace fullTimePartTime
 
         private void btnWhatStatus_Click(object sender, EventArgs e)
         {
-            if (Double.TryParse(txtCredits.Text, out double credits))
+            if (Double.TryParse(txtCredits.Text, out double credits)) //inline variable creation
             {
-                //ToDo figure out status here
-                if (credits > 12)
+                
+                if (credits > 12) //if credits entered are more than 12 assign the lblStatus.Text to full time
                 {
                     lblStatus.Text = "full time";
                 }
-                else if (credits >= 6)
+                else if (credits >= 6) //or if credits entered are equal to or greater than 6 then assign the lblStatus.Text to half time
                 {
                     lblStatus.Text = "Half time";
                 }
-                else if (credits >= 0)
+                else if (credits >= 0) //or if credits entered are greater than or equal to 0 or greater then assign the lblStatus.Text to less than half time
                 {
                     lblStatus.Text = "Less than half time";
                 }
-                else
+                else //anything else then display the MessageBox with the error below
                 {
                     MessageBox.Show("Enter a positive number", "Error");
                 }
             }
-            else
+            else //if above can not be executed because something else is entered then a MessageBox showing the error below will appear
             {
                 MessageBox.Show("Enter a number", "Error");
             }
